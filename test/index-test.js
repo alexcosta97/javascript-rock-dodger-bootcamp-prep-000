48613a2cdc27c36e69bcb7175ae422a2ec065f38
Alex Paulo da Costa <alex.callum.c@gmail.com>
2018-06-28 06:28:25 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -123,9 +123,7 @@ describe('Rock Dodger', () => {
 
   describe('endGame()', () => {
     it('clears gameInterval', () => {
-      const spy = expect.spyOn(window, 'clearInterval').andCall()
-
-      endGame()
+      const spy = expect.spyOn(window, 'clearInterval').andCall(endGame())
 
       expect(spy).toHaveBeenCalled()
     })
